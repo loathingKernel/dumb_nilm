e1b500f2c293edf04fc6591592bdd3a248dec9cc
Stelios Tsampas <loathingkernel@gmail.com>
2018-07-02 13:56:47 +0300

    New structure

--- convert_spark_to_hdf5.py
+++
@@ -1,3 +0,0 @@
-from nilm_metadata import convert_yaml_to_hdf5
-
-convert_yaml_to_hdf5('gaia_spark_dataset', 'gaia_spark_dataset.h5')
--- test.py
+++
@@ -1,9 +0,0 @@
-from nilmtk import DataSet
-from nilmtk.utils import print_dict
-from IPython.core.display import display
-
-gaia = DataSet('gaia_spark_dataset.h5')
-
-print_dict(gaia.metadata)
-print_dict(gaia.buildings)
-
